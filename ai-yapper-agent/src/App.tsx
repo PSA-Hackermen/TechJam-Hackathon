0e21e02586531f1e0e02bae05321e00abe1bc46b
pzl111 <phuazailian@gmail.com>
2025-08-30 23:03:30 +0800

    hook up api

--- ai-yapper-agent/src/App.tsx
+++ ai-yapper-agent/src/App.tsx
@@ -106,31 +106,26 @@ export function App(props: {
 
   const sendPrompt = (prompt: string) => {
     console.log("Sending prompt:", prompt);
+    setChatList(prev => [...prev, prompt]);
 
-    // const url = `https://wwkzwpjl19.execute-api.us-east-1.amazonaws.com/v1/infer`;
+    const url = `https://wwkzwpjl19.execute-api.us-east-1.amazonaws.com/v1/infer`;
 
-    // fetch(url, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'x-api-key': import.meta.env.PUBLIC_X_API_KEY as string
-    //   },
-    //   body: JSON.stringify({ "prompt": prompt, "sessionId": "session1" })
-    // })
-    //   .then(res => res.json())
-    //   .then(json => {
-    //     setData(json['response']);
-    //     console.log("Response data:", json);
-    //   })
-    //   .catch(err => {
-    //     setData("Error encountered: " + err.toString());
-    //     console.error("Error fetching data:", err);
-    //   });
-
-    const fakeResponse = "This is a fake response";
-
-    setChatList(prev => [...prev, prompt, fakeResponse, fakeResponse, fakeResponse, fakeResponse, fakeResponse]);
-
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': import.meta.env.PUBLIC_X_API_KEY as string
+      },
+      body: JSON.stringify({ "prompt": prompt, "sessionId": "session1" })
+    })
+      .then(res => res.json())
+      .then(json => {
+        setChatList(prev => [...prev, json['response']]);
+        console.log("Response data:", json);
+      })
+      .catch(err => {
+        console.error("Error fetching data:", err);
+      });
   }
 
 
